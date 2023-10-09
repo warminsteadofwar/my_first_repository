736c9c66dbdded750af400b7b0929ecaae68cb96
warminsteadofwar <144802219+warminsteadofwar@users.noreply.github.com>
2023-10-09 10:47:43 +0800

    Add files via upload

---
+++ alien_invasion/settings.py
@@ -0,0 +1,24 @@
+class Settings:
+    def __init__(self):
+        '''初始化游戏的设置'''
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # 飞船设置
+        self.ship_speed = 1.5
+        self.ship_limit = 3
+
+        # 子弹设置
+        self.bullet_speed = 2.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
+        # 外星人设置 
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 50
+        # fleet_direction 为 1 表示向右移动，为 -1 表示向左移动
+        self.fleet_direction = 1
